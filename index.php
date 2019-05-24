837eb6f9a84cc6b36e949f79bdc178a666216ca1
RickDias <rick_pereira_dias@hotmail.com>
2019-05-24 19:31:01 -0300

    Adicionando Bootstrap

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ require 'vendor/autoload.php';
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-          <link rel="stylesheet" href="view/css/index.css">
+        <link href="view/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="view/css/index.css" rel="stylesheet">
     </head>
     <body>
 
