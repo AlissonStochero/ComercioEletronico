c74a421fa2ab37dbbb677833f8024c1e3a622a51
RickDias <rick_pereira_dias@hotmail.com>
2019-05-24 19:52:36 -0300

    Font awesome add

--- index.php
+++ index.php
@@ -4,17 +4,16 @@ Code by Gerson Dias
 -->
 <?php
 require 'vendor/autoload.php';
-
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
         <link href="view/css/bootstrap.min.css" rel="stylesheet">
+        <link href="view/fontawesome/css/all.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="view/css/index.css" rel="stylesheet">
     </head>
     <body>
-
   <?php
   include 'menu.php';
 
@@ -25,6 +24,33 @@ require 'vendor/autoload.php';
     require 'view/home.php'; //primeiro acesso, padrao 'home.php'
   }
   ?>
+  <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">Copyright &copy; Iff 2019</span>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="#"><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fab fa-facebook"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fab fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li><a href="#">Sobre</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-    </body>
+  <script src="view/js/bootstrap.min.js"></script>
+
+  </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,16 +1,12 @@
 <!-- Navigation -->
 <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
     <div class="container" id="container_menu">
-        <!-- Brand and toggle get grouped for better mobile display -->
+
         <div class="navbar-header page-scroll">
           <a class="navbar-brand page-scroll" href="index.php?pag=home">Projeto Juliano</a>
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="hidden">
-                    <a href="#page-top"></a>
-                </li>
                 <li>
                     <a class="page-scroll" href="#">1</a>
                 </li>
@@ -20,12 +16,7 @@
                 <li>
                     <a class="page-scroll" href="#">3</a>
                 </li>
-                <li>
-                    <a class="page-scroll" href="#">4</a>
-                </li>
-                <li>
-                    <a class="page-scroll" href="#">5</a>
-                </li>
+              
             </ul>
         </div>
         <!-- /.navbar-collapse -->
