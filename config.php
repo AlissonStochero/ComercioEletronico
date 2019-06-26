712f161c72a4c861d2441db1f714668b6ed511fb
RickDias <rick_pereira_dias@hotmail.com>
2019-06-26 19:31:59 -0300

    Nao cria instancia da classe jqgrid com a dbconf

--- config.php
+++ config.php
@@ -1,12 +1,17 @@
 <?php
 
 // PHP Grid database connection settings, Only need to update these in new project
-
 define("PHPGRID_DBTYPE","mysql"); // mysql,oci8(for oracle),mssql,postgres,sybase
-define("PHPGRID_DBHOST","108.179.193.103");
-define("PHPGRID_DBUSER","redeb061_ce");
-define("PHPGRID_DBPASS","ze0}5*oq_o{q");
-define("PHPGRID_DBNAME","redeb061_comercio");
+// servidor local
+define("PHPGRID_DBHOST", "localhost");
+define("PHPGRID_DBUSER", "root");
+define("PHPGRID_DBPASS", "");
+define("PHPGRID_DBNAME", "comercioeletronico");
+// servidor remoto
+// define("PHPGRID_DBHOST","108.179.193.103");
+// define("PHPGRID_DBUSER","redeb061_ce");
+// define("PHPGRID_DBPASS","ze0}5*oq_o{q");
+// define("PHPGRID_DBNAME","redeb061_comercio");
 
 // Basepath for lib
 define("PHPGRID_LIBPATH",dirname(__FILE__).DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR);
--- produto/produto.php
+++ produto/produto.php
@@ -3,52 +3,52 @@ include '../config.php';
 include '../db_conf.php';
 include '../phpgrid-full-v2.1.1/lib/inc/jqgrid_dist.php';
 
-$g = new jqgrid($db_conf);
+// $g = new jqgrid($db_conf);
 
 // set few params
-$grid["caption"] = "Produtos Cadastrados";
-$grid["rowNum"] = "10";
-$grid["height"] = "";
-$g->set_options($grid);
+// $grid["caption"] = "Produtos Cadastrados";
+// $grid["rowNum"] = "10";
+// $grid["height"] = "";
+// $g->set_options($grid);
 
 // set database table for CRUD operations
-$g->table = "produto";
+// $g->table = "produto";
 
-$col = array();
-$col["title"] = "Id"; // caption of column
-$col["name"] = "id_produto";
-$col["width"] = "10";
-$cols[] = $col;
+// $col = array();
+// $col["title"] = "Id"; // caption of column
+// $col["name"] = "id_produto";
+// $col["width"] = "10";
+// $cols[] = $col;
 
 // temporary column for column placeholder comparison in formatting
-$col = array();
-$col["title"] = "Descrição"; // caption of column
-$col["name"] = "descr_produto";
-$col["width"] = "15";
-$col["editable"] = true;
-$cols[] = $col;
+// $col = array();
+// $col["title"] = "Descrição"; // caption of column
+// $col["name"] = "descr_produto";
+// $col["width"] = "15";
+// $col["editable"] = true;
+// $cols[] = $col;
 
-$col = array();
-$col["title"] = "Categoria";
-$col["name"] = "id_categoria";
-$col["dbname"] = "produto.id_categoria"; // this is required as we need to search in name field, not id
-$col["width"] = "100";
-$col["align"] = "left";
-$col["search"] = true;
-$col["editable"] = true;
-$col["edittype"] = "select"; // render as select
+// $col = array();
+// $col["title"] = "Categoria";
+// $col["name"] = "id_categoria";
+// $col["dbname"] = "produto.id_categoria"; // this is required as we need to search in name field, not id
+// $col["width"] = "100";
+// $col["align"] = "left";
+// $col["search"] = true;
+// $col["editable"] = true;
+// $col["edittype"] = "select"; // render as select
 # fetch data from database, with alias k for key, v for value
-$str = $g->get_dropdown_values("select id_categoria as k, descr_categoria as v from categoria_produto");
-$col["editoptions"] = array("value"=>":;".$str);
-$col["formatter"] = "select"; // display label, not value
-$cols[] = $col;
+// $str = $g->get_dropdown_values("select id_categoria as k, descr_categoria as v from categoria_produto");
+// $col["editoptions"] = array("value"=>":;".$str);
+// $col["formatter"] = "select"; // display label, not value
+// $cols[] = $col;
 
 //pass the cooked columns to grid
-$g->set_columns($cols);
-$g->select_command = "select produto.*, categoria_produto.descr_categoria from produto left JOIN categoria_produto on (produto.id_categoria = categoria_produto.id_categoria) ";
+// $g->set_columns($cols);
+// $g->select_command = "select produto.*, categoria_produto.descr_categoria from produto left JOIN categoria_produto on (produto.id_categoria = categoria_produto.id_categoria) ";
 
 // generate grid output, with unique grid name as 'list1'
-$out = $g->render("list1");
+// $out = $g->render("list1");
 ?>
 <html>
 <head>
@@ -56,10 +56,10 @@ $out = $g->render("list1");
     <link rel="stylesheet" type="text/css" media="screen" href="../phpgrid-full-v2.1.1/lib/js/jqgrid/css/ui.jqgrid.css"></link>
     <link rel="stylesheet" type="text/css" media="screen" href="../css/index.css"></link>
     <link rel="stylesheet" type="text/css" media="screen" href="css/produto.css"></link>
-    
+
     <link href="css/freelancer.min.css" rel="stylesheet" type="text/css"/>
     <link href="../fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"/>
-    
+
     <script src="../phpgrid-full-v2.1.1/lib/js/jquery.min.js" type="text/javascript"></script>
     <script src="../phpgrid-full-v2.1.1/lib/js/jqgrid/js/i18n/grid.locale-pt-br.js" type="text/javascript"></script>
     <script src="../phpgrid-full-v2.1.1/lib/js/jqgrid/js/jquery.jqGrid.min.js" type="text/javascript"></script>
@@ -78,9 +78,9 @@ $out = $g->render("list1");
     .ui-jqgrid tr.jqgroup, .ui-jqgrid tr.jqgrow { background-color: inherit; background-image:none; }
     </style>
 <div>
-    <h1>
-    Aqui é o produto
-    </h1>
+  <h3>
+    Cadastro de novos produtos
+  </h3>
 
 <?php
     if (isset($_GET['pag'])){
@@ -89,7 +89,7 @@ $out = $g->render("list1");
 ?>
     <div class="container_product_gerson">
     <?php
-    echo $out;
+    // echo $out;
     ?>
     </div>
 
